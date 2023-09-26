7e959ee63779ba825e9cc308e618c264921f6739
kevin-Illu <78170908+Kevin-Illu@users.noreply.github.com>
2023-09-25 22:34:03 -0600

    add:radix-themes

---
+++ src/renderer/src/pages/home/home.tsx
@@ -0,0 +1,16 @@
+import { Button, Container } from '@radix-ui/themes'
+
+const Home = () => {
+  return (
+    <Container size="1" className="w-screen h-screen">
+      <p className="">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum obcaecati voluptatem illum
+        doloribus. Nam, saepe blanditiis fuga facere voluptas quaerat eligendi. Delectus tempore
+        consequuntur optio soluta quasi numquam illum doloremque.
+      </p>
+      <Button>Hola?</Button>
+    </Container>
+  )
+}
+
+export default Home
