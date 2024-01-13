36d0194bb5ec5ee450cdba2577a0708067effec4
kevin-illu <kevinillu97@gmail.com>
2024-01-12 18:05:51 -0600

    feat(globalstate): add zustand and remove ReactContext

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,7 @@ module.exports = {
       // enable the rule specifically for TypeScript files
       files: ['*.ts'],
       rules: {
-        '@typescript-eslint/explicit-function-return-type': 'error'
+        '@typescript-eslint/explicit-function-return-type': 'off'
       }
     }
   ],
